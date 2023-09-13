fe522000dea40515d1dbc42b0b2013a345e26fba
Your Name <you@example.com>
2023-09-13 11:38:49 +0300

    f

---
+++ src/main/kotlin/ColoredPoint.kt
@@ -0,0 +1,7 @@
+class ColoredPoint(x: Double, y: Double, color1: String) : Point(x, y, color1)
+{
+    override fun toString(): String
+    {
+        return "Цветная точка: ($x, $y), Цвет: $color4"
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>)
+{
+    val point = Point(2.5,4.7)
+    println(point)
+
+    val coloredPoint = ColoredPoint(3.8,1.2, "Черный")
+    println(coloredPoint)
+
+    val line = Line(Point(1.0, 2.0), Point(3.0, 4.0))
+    line.setColor("Blue")
+    println(line)
+
+    val coloredLine = ColoredLine(Point(5.0, 6.0), Point(7.0, 8.0), "Зелёный")
+    println(coloredLine)
+
+    val polygon = Polygon(listOf(Point(2.0, 2.0), Point(4.0, 4.0), Point(6.0, 2.0)), "Жёлтый")
+    polygon.moveX(3.0)
+    println(polygon)
+}
